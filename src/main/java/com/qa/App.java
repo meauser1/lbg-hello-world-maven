ec3e99ef8d332bdd31e50681880a6c5ba22dbce2
jr <jr@example.com>
2025-03-11 13:18:32 +0000

    Change my name to print

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,7 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
-        System.out.println(sayHelloToSomeone("John"));
+        System.out.println(sayHelloToSomeone("JohnnyR"));
         System.out.println(sayGoodbye());
     }
 
